8cec14dfe25f462dd99e5c6c14d630a48fff9ac1
breadcat <breadcat@users.noreply.github.com>
2025-10-02 17:15:04 +0100

    We don't use this script any more

--- static/toggle-help.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("searchToggle");
-const div = document.getElementById("searchToggleContainer");
-button.addEventListener("click", () => {
-  div.style.display = (div.style.display === "none" || div.style.display === "")
-    ? "block"
-    : "none";
-});
